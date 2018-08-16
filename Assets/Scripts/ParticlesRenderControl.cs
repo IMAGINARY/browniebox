b3a964d66c368400bcfdbc1f0dde17cb47714783
Sebastian Uribe <suribe@gmail.com>
2018-08-16 10:16:47 +0200

    Adjust rendering bounds of particles on startup

---
+++ Assets/Scripts/ParticlesRenderControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ParticlesRenderControl : MonoBehaviour {
+
+    public GameObject cage;
+    public Material particlesMaterial;
+    public ParticleSystemRenderer particleRenderer;
+
+    void Start () {
+        var cageBounds = cage.GetComponent<BoxCollider>().bounds;
+        particlesMaterial.SetVector("_A", cageBounds.min);
+        particlesMaterial.SetVector("_B", cageBounds.max);
+        particleRenderer.material = particlesMaterial;
+    }
+}
